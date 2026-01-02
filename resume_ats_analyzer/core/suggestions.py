01c726917dbec83ffd7a26b63a48dd2fbfae6d48
niroops05 <163869521+niroops05@users.noreply.github.com>
2026-01-02 16:55:11 +0530

    Delete resume_ats_analyzer directory

--- resume_ats_analyzer/core/suggestions.py
+++
@@ -1,5 +0,0 @@
-def generate_suggestions(missing_keywords):
-    suggestions = []
-    for skill in list(missing_keywords)[:10]:
-        suggestions.append(f"Add or highlight experience related to '{skill}'.")
-    return suggestions
